e95e729daaf0bb0e6bc64685cf8e67f408756a34
abhi0126 <abhishektct@gmail.com>
2022-09-09 16:50:12 +0900

    Adding Theme for the application

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,4 +12,12 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  scrollToFooter() {
+    document.getElementById('footer')?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest'
+    })
+  }
+
 }
